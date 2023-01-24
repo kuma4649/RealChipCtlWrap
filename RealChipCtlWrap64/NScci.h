86b53506b5dba2a659ad80e6b94501a8c6bb5d1f
kumatan <kumatan@kumymelody.jp>
2023-01-24 21:35:17 +0900

    ｘ64追加

---
+++ RealChipCtlWrap64/NScci.h
@@ -0,0 +1,259 @@
+// NScci.h
+
+#pragma once
+#include "scci.h"
+#include <msclr\marshal.h>
+using namespace msclr::interop;
+
+using namespace System;
+
+namespace NScci {
+
+	public ref class NSCCI_INTERFACE_INFO
+	{ 
+
+	public:
+		NSCCI_INTERFACE_INFO();
+		~NSCCI_INTERFACE_INFO();
+		!NSCCI_INTERFACE_INFO();
+
+		SCCI_INTERFACE_INFO* SCCI_INTERFACE_INFO_;
+
+		// Interface Name
+		property String^  cInterfaceName {
+			String^ get() {
+				msclr::interop::marshal_context context;
+				return context.marshal_as<System::String^>(SCCI_INTERFACE_INFO_->cInterfaceName);
+			}
+		}
+		// Sound Chip Count;
+		property int iSoundChipCount {
+			int get() {
+				return SCCI_INTERFACE_INFO_->iSoundChipCount;
+			}
+		}
+	};
+
+	public ref class NSCCI_SOUND_CHIP_INFO
+	{
+
+	public:
+		NSCCI_SOUND_CHIP_INFO();
+		~NSCCI_SOUND_CHIP_INFO();
+		!NSCCI_SOUND_CHIP_INFO();
+
+		SCCI_SOUND_CHIP_INFO* SCCI_SOUND_CHIP_INFO_;
+
+		// Sound Chip Name
+		property String^ cSoundChipName {
+			String^ get() {
+				msclr::interop::marshal_context context;
+				return context.marshal_as<System::String^>(SCCI_SOUND_CHIP_INFO_->cSoundChipName);
+			}
+		}
+		// Sound Chip ID
+		property int iSoundChip {
+			int get() {
+				return SCCI_SOUND_CHIP_INFO_->iSoundChip;
+			}
+		}
+		// Compatible Sound Chip ID
+		property array<int>^ iCompatibleSoundChip {
+			array<int>^ get() {
+				array<int>^ a1 = gcnew array<int>(2);
+				a1[0] = SCCI_SOUND_CHIP_INFO_->iCompatibleSoundChip[0];
+				a1[1] = SCCI_SOUND_CHIP_INFO_->iCompatibleSoundChip[1];
+
+				return a1;
+			}
+		}
+		// Sound Chip clock
+		property long dClock {
+			long get() {
+				return SCCI_SOUND_CHIP_INFO_->dClock;
+			}
+		}
+		// Sound Chip clock
+		property array<long>^ dCompatibleClock {
+			array<long>^ get() {
+				array<long>^ a1 = gcnew array<long>(2);
+				a1[0] = SCCI_SOUND_CHIP_INFO_->dCompatibleClock[0];
+				a1[1] = SCCI_SOUND_CHIP_INFO_->dCompatibleClock[1];
+
+				return a1;
+			}
+		}
+		// Sound Chip Used Check
+		property Boolean bIsUsed {
+			Boolean get() {
+				return SCCI_SOUND_CHIP_INFO_->bIsUsed;
+			}
+		}
+		// 接続バスID
+		property long dBusID {
+			long get() {
+				return SCCI_SOUND_CHIP_INFO_->dBusID;
+			}
+		}
+		// サウンドロケーション
+		property long dSoundLocation {
+			long get() {
+				return SCCI_SOUND_CHIP_INFO_->dSoundLocation;
+			}
+		}
+	};
+
+	ref class NSoundInterfaceManager;
+	ref class NSoundInterface;
+	ref class NSoundChip;
+
+	//----------------------------------------
+	// Sound Interface Manager
+	//----------------------------------------
+	public ref class NSoundInterfaceManager
+	{
+	public:
+		NSoundInterfaceManager();
+		~NSoundInterfaceManager();
+		!NSoundInterfaceManager();
+
+		SoundInterfaceManager* SoundInterfaceManager_;
+
+		// ---------- LOW LEVEL APIs ----------
+		// get interface count
+		int getInterfaceCount();
+		// get interface information 
+		NSCCI_INTERFACE_INFO^ getInterfaceInfo(int iInterfaceNo);
+		// get interface instance
+		NSoundInterface^ getInterface(int iInterfaceNo);
+		// release interface instance
+		bool releaseInterface(NSoundInterface pSoundInterface);
+		// release all interface instance
+		bool releaseAllInterface();
+		// ---------- HI LEVEL APIs ----------
+		// get sound chip instance
+		NSoundChip^ getSoundChip(int iSoundChipType, long dClock);
+		// release sound chip instance
+		bool releaseSoundChip(NSoundChip pSoundChip);
+		// release all sound chip instance
+		bool releaseAllSoundChip();
+		// set delay time
+		bool setDelay(long dMSec);
+		// get delay time
+		long getDelay();
+		// reset interfaces(A sound chips initialize after interface reset)
+		bool reset();
+		// initialize sound chips
+		bool init();
+		// Sound Interface instance initialize
+		bool initializeInstance();
+		// Sound Interface instance release
+		bool releaseInstance();
+		//// config scci
+		//// !!!this function is scciconfig exclusive use!!!
+		//bool config();
+		// get version info
+		long getVersion(long% pMVersion);
+		// get Level mater disp valid
+		bool isValidLevelDisp();
+		// get Level mater disp visible
+		bool isLevelDisp();
+		// set Level mater disp visible
+		void setLevelDisp(bool bDisp);
+		// set mode
+		void setMode(int iMode);
+		// send datas
+		void sendData();
+		// clear buffer
+		void clearBuff();
+		// set Acquisition Mode(Sound Chip)
+		void setAcquisitionMode(int iMode);
+		// set Acquisition Mode clock renge
+		void setAcquisitionModeClockRenge(long dClock);
+		// set command buffer size
+		bool setCommandBuffetSize(long dBuffSize);
+		// buffer check
+		bool isBufferEmpty();
+
+	};
+
+	//----------------------------------------
+	// Sound Interface(LOW level APIs)
+	//----------------------------------------
+	public ref class NSoundInterface
+	{
+	public:
+		NSoundInterface();
+		~NSoundInterface();
+		!NSoundInterface();
+
+		SoundInterface* SoundInterface_;
+
+		// support low level API check
+		Boolean isSupportLowLevelApi();
+		// send data to interface
+		Boolean setData(array<Byte>^ pData, long dSendDataLen);
+		// get data from interface
+		long    getData(array<Byte>^ pData, long dGetDataLen);
+		// set delay time
+		Boolean setDelay(long dDelay);
+		// get delay time
+		long    getDelay();
+		// reset interface
+		Boolean reset();
+		// initialize sound chips
+		Boolean init();
+		// サウンドチップ数取得
+		long	getSoundChipCount();
+		// サウンドチップ取得
+		NSoundChip^ getSoundChip(long dNum);
+	};
+
+	//----------------------------------------
+	// Sound Chip
+	//----------------------------------------
+	public ref class NSoundChip
+	{
+
+	public:
+		NSoundChip();
+		~NSoundChip();
+		!NSoundChip();
+
+		SoundChip* SoundChip_;
+
+		// get sound chip information
+		NSCCI_SOUND_CHIP_INFO^ getSoundChipInfo();
+		// get sound chip type
+		int getSoundChipType();
+		// set Register data
+		Boolean setRegister(long dAddr, long dData);
+		// get Register data(It may not be supported)
+		long getRegister(long dAddr);
+		// initialize sound chip(clear registers)
+		Boolean init();
+		// get sound chip clock
+		long getSoundChipClock();
+		// get writed register data
+		long getWrittenRegisterData(long addr);
+		// buffer check
+		Boolean isBufferEmpty();
+	};
+
+	public ref class NScci
+	{
+		SoundInterfaceManager* SoundInterfaceManager_;
+		SoundInterface* SoundInterface_;
+		SoundChip* SoundChip_;
+
+	public:
+		NScci();
+		~NScci();
+		!NScci();
+
+		NSoundInterfaceManager^ NSoundInterfaceManager_;
+		NSoundInterface^ NSoundInterface_;
+		NSoundChip^ NSoundChip_;
+	};
+
+}
---
+++ RealChipCtlWrap64/Nc86ctl.cpp
@@ -0,0 +1,211 @@
+#include "stdafx.h"
+#include "Nc86ctl.h"
+#include <msclr\marshal.h>
+using namespace msclr::interop;
+
+HMODULE	hC86ctl = NULL;
+typedef HRESULT(WINAPI *TCreateInstance)(REFIID, interior_ptr<c86ctl::IRealChipBase>);
+
+Nc86ctl::Nc86ctl::Nc86ctl()
+{
+	if (hC86ctl == NULL) {
+		hC86ctl = LoadLibraryA("c86ctl.dll");
+	}
+
+	if (hC86ctl == NULL) {
+		::FreeLibrary(hC86ctl);
+		return;
+	}
+
+	TCreateInstance pCreateInstance = (TCreateInstance)(::GetProcAddress(hC86ctl, "CreateInstance"));
+	if (pCreateInstance == NULL) {
+		::FreeLibrary(hC86ctl);
+		return;
+	}
+
+	pCreateInstance(c86ctl::IID_IRealChipBase, (interior_ptr<c86ctl::IRealChipBase>)&pChipBase);
+	pChipBase->initialize();
+}
+
+Nc86ctl::Nc86ctl::~Nc86ctl()
+{
+	this->!Nc86ctl();
+}
+
+Nc86ctl::Nc86ctl::!Nc86ctl()
+{
+	hC86ctl = NULL;
+	::FreeLibrary(hC86ctl);
+}
+
+int Nc86ctl::Nc86ctl::initialize()
+{
+	return pChipBase->initialize();
+}
+
+int Nc86ctl::Nc86ctl::deinitialize()
+{
+	return pChipBase->deinitialize();
+}
+
+Nc86ctl::NIRealChip^ Nc86ctl::Nc86ctl::getChipInterface(int id)
+{
+	if (pChipBase == nullptr) {
+		throw gcnew Exception();
+	}
+
+	c86ctl::IRealChip * pRC;
+	pChipBase->getChipInterface(id, c86ctl::IID_IRealChip, (void**)&pRC);
+	if (pRC == nullptr) {
+		return nullptr;
+	}
+
+	NIRealChip^ nirc = gcnew NIRealChip();
+	nirc->pRC = pRC;
+
+	return nirc;
+}
+
+int Nc86ctl::Nc86ctl::getNumberOfChip()
+{
+	return pChipBase->getNumberOfChip();
+}
+
+
+
+
+Nc86ctl::NIRealChip::NIRealChip()
+{
+}
+
+Nc86ctl::NIRealChip::~NIRealChip()
+{
+	this->!NIRealChip();
+}
+
+Nc86ctl::NIRealChip::!NIRealChip()
+{
+}
+
+void Nc86ctl::NIRealChip::reset()
+{
+	pRC->reset();
+}
+
+void Nc86ctl::NIRealChip::out(UInt16 adr,byte data)
+{
+	pRC->out(adr, data);
+}
+
+byte Nc86ctl::NIRealChip::in(UInt16 adr)
+{
+	return pRC->in(adr);
+}
+
+Nc86ctl::NIGimic2^ Nc86ctl::NIRealChip::QueryInterface()
+{
+	if (pRC == nullptr) {
+		throw gcnew Exception();
+	}
+
+	c86ctl::IGimic2 * pIGimic;
+	pRC->QueryInterface(c86ctl::IID_IGimic2, (void**)&pIGimic);
+
+	if (pIGimic == nullptr) {
+		return nullptr;
+	}
+
+	NIGimic2^ nig = gcnew NIGimic2();
+	nig->pIGimic = pIGimic;
+
+	return nig;
+}
+
+
+
+Nc86ctl::NIGimic2::NIGimic2()
+{
+}
+
+Nc86ctl::NIGimic2::~NIGimic2()
+{
+	this->!NIGimic2();
+}
+
+Nc86ctl::NIGimic2::!NIGimic2()
+{
+}
+
+int Nc86ctl::NIGimic2::getFWVer(UInt16% major, UInt16% minor, UInt16% revision, UInt16% build)
+{
+	UINT ma;
+	UINT mi;
+	UINT rev;
+	UINT bui;
+	pIGimic->getFWVer(&ma, &mi, &rev, &bui);
+	major = (UInt16)ma;
+	minor = (UInt16)mi;
+	revision = (UInt16)rev;
+	build = (UInt16)bui;
+	return 0;
+}
+
+Nc86ctl::Devinfo ^ Nc86ctl::NIGimic2::getMBInfo()
+{
+	c86ctl::Devinfo di;
+	pIGimic->getMBInfo(&di);
+
+	Devinfo^ inf = gcnew Devinfo();
+	msclr::interop::marshal_context context;
+	inf->Devname = context.marshal_as<System::String^>(di.Devname);
+	inf->Rev = di.Rev;
+	inf->Serial = context.marshal_as<System::String^>(di.Serial);
+
+	return inf;
+}
+
+Nc86ctl::ChipType Nc86ctl::NIGimic2::getModuleType()
+{
+	c86ctl::ChipType tp;
+	pIGimic->getModuleType(&tp);
+
+	return (ChipType)(int)tp;
+}
+
+Nc86ctl::Devinfo^ Nc86ctl::NIGimic2::getModuleInfo()
+{
+	c86ctl::Devinfo di;
+	pIGimic->getModuleInfo(&di);
+
+	Devinfo^ inf = gcnew Devinfo();
+	msclr::interop::marshal_context context;
+	inf->Devname = context.marshal_as<System::String^>(di.Devname);
+	inf->Rev = di.Rev;
+	inf->Serial = context.marshal_as<System::String^>(di.Serial);
+
+	return inf;
+}
+
+void Nc86ctl::NIGimic2::setSSGVolume(byte vol)
+{
+	pIGimic->setSSGVolume(vol);
+}
+
+byte Nc86ctl::NIGimic2::getSSGVolume()
+{
+	UCHAR vol;
+	pIGimic->getSSGVolume(&vol);
+	return vol;
+}
+
+void Nc86ctl::NIGimic2::setPLLClock(UInt32 clock)
+{
+	pIGimic->setPLLClock(clock);
+}
+
+UInt32 Nc86ctl::NIGimic2::getPLLClock()
+{
+	UINT clock;
+	pIGimic->getPLLClock(&clock);
+	return clock;
+}
